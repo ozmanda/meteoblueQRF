e800e511b94c7eb72365b76f19c34120ba59c877
ozmanda <usherjul@students.zhaw.ch>
2022-05-09 11:50:11 +0200

    Finished error estimation and created save_output method of DropsetQRF class

--- DropsetQRF.py
+++ DropsetQRF.py
@@ -1,10 +1,14 @@
+import os
+import numpy as np
 from pandas import DataFrame, concat
 from quantile_forest import RandomForestQuantileRegressor
 
 class DropsetQRF():
-    def __init__(self, datasets):
+    def __init__(self, datasets, CI):
         self.data = datasets
         self.stations = datasets.keys()
+        self.lowerCI = (100-CI) / 2
+        self.upperCI = 100 - self.lowerCI
 
     def xy_generation(self, testkey):
         # separate test from training data
@@ -26,13 +30,31 @@ class DropsetQRF():
         return xTrain.dropna(), xTest, yTrain, yTest
 
     def run_error_estimation(self):
-        MSE = {}
-        insideCI = {}
+        self.Output = {}
         for key in self.stations:
+            stationData = {}
             xTrain, xTest, yTrain, yTest = self.xy_generation(key)
             qrf = RandomForestQuantileRegressor()
             qrf.fit(xTrain, yTrain)
 
+            # predict test set
             yPred = qrf.predict(xTest, quantiles=[0.025, 0.5, 0.975])
-            MSE['key'] =
 
+            # calculate relevant error metrics and fill into dictionary
+            stationData['True Temperature'] = yTest
+            stationData['Predicted Temperature'] = yPred[:, 1]
+            stationData['2.5%'] = yPred[:, 0]
+            stationData['97.5%'] = yPred[:, 2]
+            stationData['Absolute Deviation'] = np.abs(yTest - yPred[:, 1])
+            stationData['MSE'] = np.sum(stationData['Absolute Deviation'] ** 2) / len(stationData['Absolute Deviation'])
+
+            # enter station dictionary into ouput dictionary
+            self.Output[key] = stationData
+
+    def save_output(self, savepath):
+        if not os.path.isdir(savepath):
+            os.mkdir(savepath)
+        for key in self.Output:
+            MSE = self.Output[key].pop('MSE')
+            df = DataFrame(self.Output[key])
+            df.to_csv(os.path.join(savepath, f'{key}_MSE_{np.round(MSE, 2)}.csv'))
--- main.py
+++ main.py
@@ -9,25 +9,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--stationDatapath', help='absolute path to folder containing station data',
                         default='C://Users//ushe//PycharmProjects//Stadtklima QRF//Data//MeasurementFeatures_v6')
+    parser.add_argument('--savefolder', help='absolute path to the desired save folder, which will be created if it '
+                                             'does not already exist',
+                        default='C://Users//ushe//PycharmProjects//pythonQRF//Data//DropsetData')
     parser.add_argument('--starttime', help='Date and time of the beginning of the data interval in the format'
                                             'YYYY/MM/DD HH:MM', default='2019/06/25 00:00')
     parser.add_argument('--endtime', help='Date and time of the end of the data interval in the format'
                                           'YYYY/MM/DD HH:MM', default='2019/06/29 00:00')
+    parser.add_argument('--CI', help='Confidence interval in percent (i.e. 95 for the 95% CI', default=95)
 
     args = parser.parse_args()
-    # datasets = utils.load_data(args.stationDatapath, startDatetime=args.starttime, endDatetime=args.endtime)
-    # dropsetQRF = DropsetQRF(datasets)
-    # errors = dropsetQRF.run_error_estimation()
-    #
+    datasets = utils.load_data(args.stationDatapath, startDatetime=args.starttime, endDatetime=args.endtime)
+    dropsetQRF = DropsetQRF(datasets, args.CI)
+    dropsetQRF.run_error_estimation()
+    dropsetQRF.save_output(args.savefolder)
 
-    from quantile_forest import RandomForestQuantileRegressor
-    from sklearn import datasets
-
-    x, y = datasets.fetch_california_housing(return_X_y=True)
-
-    qrf = RandomForestQuantileRegressor()
-    qrf.fit(x, y)
-
-    yPred = qrf.predict(x, quantiles=[0.025, 0.5, 0.975])
-
-    x = 5
